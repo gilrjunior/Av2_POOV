71fcdf215c6cb6bd21823f3fb63e3ffcea308a7d
gilrjunior <gilmarjunioreis@gmail.com>
2022-12-03 15:14:45 -0300

    Formatação

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,20 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package cliente;
 
-/**
- *
- * @author User
- */
-public class Cliente {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Cliente {
+    
+    private int IdCliente;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(int IdCliente, String nome, String email, String telefone, String endereco) {
+        this.IdCliente = IdCliente;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
     }
+        
+        public Cliente() {
+        this.IdCliente = 0;
+        this.nome = "";
+        this.email = "";
+        this.telefone = "";
+        this.endereco = "";
+    }
+
+    public int getIdCliente() {
+        return IdCliente;
+    }
+
+    public void setIdCliente(int IdCliente) {
+        this.IdCliente = IdCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "IdCliente=" + IdCliente + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", endereco=" + endereco;
+    }
+
     
 }
